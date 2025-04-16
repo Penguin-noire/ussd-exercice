f6b366bfa57469b4f7a7d46be35da43ff366878c
Penguin-noire <igiranezanellyjersy2@gmail.com>
2025-04-16 09:55:47 +0200

    Update add.py

--- add.py
+++ add.py
@@ -14,15 +14,14 @@ def charger_donnees():
     except json.JSONDecodeError:
         return []  e
 
-# Fonction pour sauvegarder des données dans le fichier
+
 def sauvegarder_donnees(donnees):
     with open(fichier_json, 'w') as f:
         json.dump(donnees, f, indent=4)
 
-# Charger les données existantes
+
 donnees = charger_donnees()
 
-# Ajouter de nouvelles données
 nouvelle_donnee = {
     "id": input("Shiramwo ikidandazwa id : "),
     "izina": input("Izina ry'ikidandanzwa : "),
@@ -32,7 +31,7 @@ nouvelle_donnee = {
 
 donnees.append(nouvelle_donnee)
 
-# Sauvegarder les données mises à jour
+
 sauvegarder_donnees(donnees)
 
 print("Les données ont été ajoutées avec succès.")
